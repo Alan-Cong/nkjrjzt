b91438662635470efbecd9f9ed6e0e8f8c507ea2
Alan-Cong <3302932711@qq.com>
2025-03-04 14:10:23 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// 页面加载完成后绑定交互事件
+document.addEventListener('DOMContentLoaded', function() {
+  // “了解更多”按钮点击后执行的函数
+  window.showMoreInfo = function() {
+    alert("感谢关注南开大学金融学院记者团，我们致力于记录精彩的校园生活！");
+  };
